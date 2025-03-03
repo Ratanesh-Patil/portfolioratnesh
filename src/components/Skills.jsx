b0464c4e24a6b51eec72c365ae3a09c58fe3cc7c
unknown <patil.ratnesh1997@gmail.com>
2025-03-03 23:25:43 +0530

    skills add

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,25 +1,27 @@
 import {  motion } from 'framer-motion'
-import { FaJava, FaReact, FaDatabase,  FaGithub } from 'react-icons/fa'
+import { FaJava, FaReact, FaDatabase,  FaGithub, FaGitAlt } from 'react-icons/fa'
 import { RiTailwindCssFill } from 'react-icons/ri';
 import { SiSpringboot, SiPostgresql, SiJavascript, SiHtml5, SiPostman, } from 'react-icons/si'
-import { TbBrandMysql, TbBrandReactNative } from 'react-icons/tb';
+import { TbBrandMysql, TbBrandReactNative, TbBrandRedux } from 'react-icons/tb';
 import { VscVscode } from 'react-icons/vsc';
 
 export const Skills = () => {
   const skillsData = [
     { name: 'Java', icon: FaJava ,color: 'text-red-500'},
-    { name: 'React', icon: FaReact ,color: 'text-blue-400'},
     { name: 'Spring Boot', icon: SiSpringboot , color: 'text-green-500'},
-    { name: 'PostgreSQL', icon: SiPostgresql ,  color: 'text-blue-500'},
+    { name: 'React', icon: FaReact ,color: 'text-blue-400'},
+    { name: 'Redux', icon: TbBrandRedux ,color: 'text-purple-400'},
     { name: 'JavaScript', icon: SiJavascript , color: 'text-yellow-400'},
+    { name: 'Tailwind CSS', icon: RiTailwindCssFill , color: 'text-blue-400'},
     { name: 'HTML/CSS', icon: SiHtml5 , color: 'text-red-400'},
-    { name: 'RESTful APIs', icon: FaDatabase ,  color: 'text-green-400'},
+    { name: 'React Native', icon: TbBrandReactNative , color: 'text-blue-400'},
+    { name: 'PostgreSQL', icon: SiPostgresql ,  color: 'text-blue-500'},
     { name: 'MySQL', icon: TbBrandMysql , color: 'text-blue-500'},
     { name: 'Postman', icon: SiPostman , color: 'text-orange-400'},
+    { name: 'RESTful APIs', icon: FaDatabase ,  color: 'text-green-400'},
     { name: 'VsCode', icon: VscVscode , color: 'text-blue-500'},
     { name: 'GitHub ', icon: FaGithub , color: 'text-black-500'},
-    { name: 'Tailwind CSS', icon: RiTailwindCssFill , color: 'text-blue-400'},
-    { name: 'React Native', icon: TbBrandReactNative , color: 'text-blue-400'},
+    { name: 'Git', icon: FaGitAlt , color: 'text-orange-500'},
   ]
 
   return (
